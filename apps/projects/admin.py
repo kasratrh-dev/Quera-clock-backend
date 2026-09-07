bcd15ef6aee3453b280c41250bade07b45eef5ce
Kasra <kasratrh@gmail.com>
2026-09-07 18:17:51 +0330

    feat:add project admin

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.admins.base import SoftDeleteAdmin
+from .models import Project
 
-# Register your models here.
+
+
+
+@admin.register(Project)
+class ProjectAdmin(SoftDeleteAdmin):
+    list_display = ('workspace' , 'name' , 'client' , 'is_archived')
+    search_fields = ('name',)
+    list_filter = ('workspace',)
+    ordering = ('-created_at',)
