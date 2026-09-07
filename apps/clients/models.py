332a7c566800e2458ed617a2566ea064d7c1df11
Kasra <kasratrh@gmail.com>
2026-09-07 17:12:25 +0330

    feat: Client model done!

---
+++ apps/clients/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from core.models import TimestampedModel
+from apps.workspaces.models import Workspace
+
+
+
+class Client(TimestampedModel):
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE,related_name='clients')
+    name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True)
+    notes = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='clients/',blank=True)
+
+
+    class Meta:
+        ordering = ('name',)
+        constraints = [
+            models.UniqueConstraint(fields=('workspace','name')
+                                    ,condition=models.Q(is_deleted=False),
+                                    name='unique_client_name_per_workspace'),
+
+        ]
+
+    def __str__(self):
+        return self.name
--- config/settings/base.py
+++ config/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'apps.users.apps.UsersConfig',
     'apps.workspaces.apps.WorkspacesConfig',
+    'apps.clients.apps.ClientsConfig',
 
 ]
 
