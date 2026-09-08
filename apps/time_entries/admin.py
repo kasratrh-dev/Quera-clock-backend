342a84ed732af5aecccb7ec06d9ebfc8686b6ced
Kasra <kasratrh@gmail.com>
2026-09-08 13:45:18 +0330

    feat: add admin TimeEntry

--- apps/time_entries/admin.py
+++ apps/time_entries/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admins.base import SoftDeleteAdmin
+from .models import TimeEntry
+
+
+@admin.register(TimeEntry)
+class TimeEntryAdmin(SoftDeleteAdmin):
+    list_display = (
+        "workspace",
+        "user",
+        "project",
+        "start_time",
+        "end_time",
+    )
+
+    search_fields = (
+        "description",
+    )
+
+    list_filter = (
+        "workspace",
+        "project",
+        "start_time",
+    )
+
+    ordering = (
+        "-created_at",
+    )
